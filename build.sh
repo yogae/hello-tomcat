afcd930bf24cf74750ae570891fc6374840070bf
kim jihyun <>
2019-08-21 08:43:46 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,11 +1,9 @@
 #!bin/bash
 
-export CLASSPATH=/Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/lib/servlet-api.jar
-
 CPATH=$(pwd)
+export CLASSPATH=$CPATH/lib/servlet-api.jar
 DIST_PATH=$CPATH/dist
 
 cd $CPATH/src && javac *.java
 cd $CPATH/src && mv *.class $DIST_PATH/WEB-INF/classes/
 cd $DIST_PATH && jar cfM ROOT.war *
-# cd $CPATH && docker-compose up --build
--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,11 @@ source ./.env
 cd $CPATH/codedeploy && zip -r ../data.zip ./* 
 cd $CPATH
 
-aws s3 cp data.zip s3://$S3_BUCKET/data.zip --profile jihyun
+aws s3 cp data.zip s3://$S3_BUCKET/data.zip
 
 aws deploy create-deployment \
   --application-name $APP_NAME \
   --deployment-config-name CodeDeployDefault.OneAtATime \
   --deployment-group-name $DEPLOYMENT_GROUP_NAME \
   --description "s3 deployment demo" \
-  --s3-location bucket=$S3_BUCKET,bundleType=zip,key=data.zip \
-  --profile jihyun
+  --s3-location bucket=$S3_BUCKET,bundleType=zip,key=data.zip
